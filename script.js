a2564e1cef975b14fd8b5319a8a34d2e0a8b59b4
Miraziz <rikhsiboyevmiraziz@gmail.com>
2020-12-14 16:33:01 +0500

    16:32

---
+++ script.js
@@ -0,0 +1,27 @@
+var name = prompt('Ismingizni kiriting')
+console.log(name)
+
+var age = prompt('Yoshingizni kiriting')
+console.log(age)
+
+var plus = prompt('javobni kiriting 5+2=?')
+var javob = 'togri javob ->' + (5+2)
+console.log('sizning javob ->' + plus +javob)
+
+var minus = prompt('javobni kiriting 8-5=?')
+var javob2 = 'togri javob ->' + (8-5)
+console.log('sizning javob ->' + minus + javob2)
+
+var up = prompt('javobni kiriting 6*8=?')
+var javob3 = 'togri javob ->' + (6*8)
+console.log('sizning javob ->' + up + javob3)
+
+var slash = prompt('javobni kiriting 45/9=?')
+var javob4 = 'togri javob ->' + (45/9)
+console.log('sizning javob ->' + slash + javob4)
+
+var qoldiq =prompt('qoldiqni toping 8/3=?') 
+var javob5 = 'togri javob ->' + (2)
+console.log('sizning javob ->' + qoldiq + javob5)
+
+alert('Natijani korish uchun consolega oting')
